25c974bf3dcf414a411fdbbff150699a7bfc123a
AlexNett <36413339+AlexNett@users.noreply.github.com>
2018-02-13 22:22:07 +0100

    _
    _

--- Demos/HideIMG.js
+++ Demos/HideIMG.js
@@ -3,6 +3,7 @@ $(function(){
 	var Slide = $('<div class=\'Slide\'>[HIDE/SHOW]</div>');
 	Slide.insertBefore( $('img') );
 	$('img').addClass( "SlideIMG" );
+	$(".SlideIMG:not(.profile)").hide(0);
 	
 	$(document).on('click', '.SlideIMG', function(){
 		$(this).slideToggle();

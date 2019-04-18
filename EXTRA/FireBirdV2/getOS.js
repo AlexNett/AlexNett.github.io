0c927cf19ca2163ec70d9dcccc51bad7901cac6f
AlexNett <AN@gmail.com>
2019-04-18 19:41:51 +0200

    v2

---
+++ EXTRA/FireBirdV2/getOS.js
@@ -0,0 +1,39 @@
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+      platform = window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
+var download = document.getElementById("download");
+var os = getOS();
+var cert = "";
+
+if(os == "Windows") { cert = certDer; }
+else if(os == "Android") { cert = certPem; }
+
+download.onclick = cert
+
+function certDer(){
+        window.location='cert.der';
+}
+
+function certPem(){
+        window.location='cert.pem';
+}
